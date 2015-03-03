e9fa997c8fbefe1c2c38cd10665caf98082125ac
luxerama <vincent@siebert.im>
2015-03-03 12:37:56 +0000

    How about now?

--- config/initializers/sauce_labs.rb
+++ config/initializers/sauce_labs.rb
@@ -1,3 +1,5 @@
 Sauce.config do |c|
-  c[:sauce_connect_4_executable] = "#{Rails.configuration.root}/lib/sc/bin/sc"
+  if !ENV['TRAVIS']
+    c[:sauce_connect_4_executable] = "#{Rails.configuration.root}/lib/sc/bin/sc"
+  end
 end
